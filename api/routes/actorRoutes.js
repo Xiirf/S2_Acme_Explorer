ea6853a5faa26660e7cff40e7afe4fbf9a48b637
Pierre-François Giraud <pierrefrancois.giraud@insa-rouen.fr>
2020-02-05 14:35:03 +0100

    Add actor Model, controller and routes

---
+++ api/routes/actorRoutes.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(app) {
+    var actors = require('../controllers/actorController');
+    
+    app.route('/actors')
+        .get(actors.list_all_actors)
+    app.route('actor')
+        .post(actors.create_an_actor)
+    app.route('actor/:actorId')
+        .post(actors.read_an_actor)
+        .put(actors.edit_an_actor)
+        .delete(actors.delete_an_actor)        
+}
