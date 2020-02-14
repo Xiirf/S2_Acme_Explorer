a6cc0ae78373218e0b22643ff102e782d073d5a4
Flavien Cocu <Flavien.cocu@insa-rouen.fr>
2020-02-14 12:50:50 +0100

    add plural in routes

--- api/routes/stageRoutes.js
+++ api/routes/stageRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     app.route('/stages')
         .get(stages.list_all_stages)
         .post(stages.create_a_stage)
-    app.route('/stage/:stageId')
+    app.route('/stages/:stageId')
         .get(stages.read_a_stage)
         .put(stages.edit_a_stage)
         .delete(stages.delete_a_stage)        
--- api/routes/tripRoutes.js
+++ api/routes/tripRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     app.route('/trips')
         .get(trips.list_all_trips)
         .post(trips.create_a_trip)
-    app.route('/trip/:tripId')
+    app.route('/trips/:tripId')
         .get(trips.read_a_trip)
         .put(trips.edit_a_trip)
         .delete(trips.delete_a_trip)        
